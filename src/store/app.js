1c40a3b0d135f4ee8f431f3ccfac54ae1d95cc29
Mustafa GÜNEY <mail@mustafaguney.com>
2023-02-16 03:44:24 +0300

    Title & tag filters has been added

--- src/store/app.js
+++ src/store/app.js
@@ -1,53 +1,117 @@
 // Utilities
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useAppStore = defineStore('app', {
-  state: () => ({
-  }),
-})
+export const useAppStore = defineStore("app", {
+  state: () => ({}),
+});
 
-export const useTodos = defineStore('todos', {
+export const useTodosStore = defineStore("todos", {
   state: () => ({
     todoList: [
       {
         id: 0,
-        title: 'Make a breakfast',
-        tags: ['breakfast', 'berry', 'yogurt', 'rasted potato'],
-        endDate: 'Thu Feb 16 2023 20:59:00 GMT+0300 (GMT+03:00)',
+        title: "Make a breakfast",
+        tags: ["breakfast", "berry", "yogurt", "rasted potato"],
+        endDate: "Thu Feb 16 2023 20:59:00 GMT+0300 (GMT+03:00)",
         state: false,
-        description:'Make a breakfast in en vie restaurant at 22:00pm'
+        description: "Make a breakfast in en vie restaurant at 22:00pm",
       },
       {
         id: 1,
-        title: 'Go to gym',
-        tags: ['gym', 'yoga', 'run'],
-        endDate: 'Thu Feb 18 2023 20:59:00 GMT+0300 (GMT+03:00)',
+        title: "Go to gym",
+        tags: ["gym", "yoga", "run"],
+        endDate: "Thu Feb 18 2023 20:59:00 GMT+0300 (GMT+03:00)",
         state: true,
-        description:'Go to the gym and run 30km'
+        description: "Go to the gym and run 30km",
       },
       {
         id: 2,
-        title: 'Go to work',
-        tags: ['work', 'jobs', 'run'],
-        endDate: 'Thu Feb 13 2023 20:59:00 GMT+0300 (GMT+03:00)',
+        title: "Go to work",
+        tags: ["work", "jobs", "run"],
+        endDate: "Thu Feb 13 2023 20:59:00 GMT+0300 (GMT+03:00)",
         state: true,
-        description:'Go to the work'
-      }
-    ]
+        description: "Go to the work",
+      },
+
+    ],
   }),
   getters: {
-    completedTodos(state) {
-      return state.todos.filter((items) => items.state == true)
+    getTags: (state) => {
+      let tags = [];
+      for (let i = 0; i < state.todoList.length; i++) {
+        for (let x = 0; x < state.todoList[i].tags.length; x++) {
+          !tags.includes(state.todoList[i].tags[x]) &&
+            tags.push(state.todoList[i].tags[x]);
+        }
+      }
+      return tags.sort();
     },
-    unCompletedTodos(state) {
-      return state.todos.filter((items) => items.state == false)
+    filterByTitle: (state) => {
+      return (title) =>
+        state.todoList.filter((items) =>
+          items.title.toLowerCase().includes(title)
+        );
     },
-    filterByTitle(state, value) {
-      return state.todos.filter((items)=> items.title.includes(value))
+    filterAll: (state) => {
+      return (tags, title) => {
+        let filteredTodoList = [];
+        let tagList = tags ? tags : "";
+        if (tags && tags.length > 0) {
+          for (let i = 0; i < state.todoList.length; i++) {
+            for (let x = 0; x < tagList.length; x++) {
+              filteredTodoList.filter((item) => item.id == state.todoList[i].id)
+                .length < 1 &&
+                state.todoList[i].tags.filter((items) =>
+                  items.includes(tagList[x])
+                ).length > 0 &&
+                filteredTodoList.push(state.todoList[i]);
+            }
+          }
+        } else {
+          filteredTodoList = state.todoList;
+        }
+        return title
+          ? filteredTodoList.filter((items) => items.title.toLowerCase().includes(title))
+          : filteredTodoList;
+      };
     },
-    filterByTags(state, value) {
-      return state.todos.filter((items)=> items.tags.includes(value))
-    }
-  }
-})
-
+    filterCompleted(state) {
+      return (tags, title) => {
+        let filteredTodoList = [];
+        let tagList = tags;
+        console.log("tags", tagList);
+        for (let i = 0; i < state.todoList.length; i++) {
+          for (let x = 0; x < tagList.length; x++) {
+            state.todoList[i].state == true &&
+              state.todoList[i].tags.filter((items) =>
+                items.includes(tagList[x])
+              ).length > 0 &&
+              filteredTodoList.push(state.todoList[i]);
+          }
+        }
+        return title
+          ? filteredTodoList.filter((items) => items.title.includes(title))
+          : filteredTodoList;
+      };
+    },
+    filterUncompleted(state) {
+      return (tags, title) => {
+        let filteredTodoList = [];
+        let tagList = tags;
+        console.log("tags", tagList);
+        for (let i = 0; i < state.todoList.length; i++) {
+          for (let x = 0; x < tagList.length; x++) {
+            state.todoList[i].state == false &&
+              state.todoList[i].tags.filter((items) =>
+                items.includes(tagList[x])
+              ).length > 0 &&
+              filteredTodoList.push(state.todoList[i]);
+          }
+        }
+        return title
+          ? filteredTodoList.filter((items) => items.title.includes(title))
+          : filteredTodoList;
+      };
+    },
+  },
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,24 +2,55 @@
   <div>
     <v-toolbar class="elevation-1">
       <v-toolbar-title>To-Do List</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+           v-model="titleParam"
+          outlined
+          clearable
+          class="font-weight-bold"
+          prepend-inner-icon="mdi-magnify"
+          label="Search"
+          hide-details
+        ></v-text-field>
+          <v-spacer></v-spacer>
+          <v-select
+          outlined
+          v-model="tagsParam"
+          :items="store.getTags"
+          class="align-stretch font-weight-bold"
+          item-text="name"
+          chips
+          pag
 
-      <v-btn icon>
+          width="100px"
+          item-value="id"
+          label="Tag Filter"
+          multiple
+          hide-details
+        >
+         <!-- <template v-slot:selection="{ item, index }">
+            <v-chip v-if="index < 3">
+              <span>{{ item }}</span>
+            </v-chip>
+            <span v-else class="grey--text text-caption">
+              (+{{ item.length - 3 }})
+            </span>
+          </template> -->
+        </v-select>
+      <!-- <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-
       <v-btn icon>
         <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
+      </v-btn> -->
     </v-toolbar>
     <v-data-table
-      v-model="selected"
       :headers="headers"
       hide-default-footer
-      items-per-page="-1"
-      :items="todoList"
-       :sort-by="[{ key: 'state', order: 'asc' }, { key: 'endDate', order: 'desc' }]"
+      :items="store.filterAll(tagsParam, titleParam.toLowerCase())"
+      :sort-by="[{ key: 'state', order: 'asc' }, { key: 'endDate', order: 'desc' }]"
       item-value="id"
+       items-per-page="-1"
       class="elevation-1 mt-4"
     >
       <template v-slot:[`item.state`]="{ item }">
@@ -50,12 +81,13 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { useDisplay } from "vuetify";
-import { useTodos } from "@/store/app";
+import { useTodosStore } from "@/store/app";
+import { storeToRefs } from 'pinia'
 import { VDataTable } from "vuetify/labs/VDataTable";
-import checkTask from "@/components/checkTask";
-const store = useTodos();
-const todoList = ref(store.todoList);
+const store = useTodosStore();
 const { mobile } = useDisplay();
+const titleParam = ref("");
+const tagsParam = ref();
 const headers = ref([
   { title: "State", width: "90", key: "state" },
   { title: "Title", key: "title", sortable:false},
@@ -64,6 +96,7 @@ const headers = ref([
   { title: "Description", key: "description", sortable:false },
 ]);
 const selected = ref([]);
+
 </script>
 <style>
 .v-data-table-footer{
